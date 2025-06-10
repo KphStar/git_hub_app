8d160642e2215305ee9626da49aeff218b6b968a
KphStar <37385165+KphStar@users.noreply.github.com>
2025-06-09 22:45:15 -0700

    added icon

--- User_RepoProp.Designer.cs
+++ User_RepoProp.Designer.cs
@@ -28,17 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(User_RepoProp));
             this.BtnExit = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.labelTitle = new System.Windows.Forms.Label();
+            this.flowPanelFiles = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnExit
             // 
-            this.BtnExit.Location = new System.Drawing.Point(826, 478);
+            this.BtnExit.Location = new System.Drawing.Point(872, 531);
             this.BtnExit.Name = "BtnExit";
-            this.BtnExit.Size = new System.Drawing.Size(260, 64);
+            this.BtnExit.Size = new System.Drawing.Size(260, 49);
             this.BtnExit.TabIndex = 0;
             this.BtnExit.Text = "EXIT";
             this.BtnExit.UseVisualStyleBackColor = true;
@@ -47,28 +49,40 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 125);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 531);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(742, 292);
+            this.dataGridView1.Size = new System.Drawing.Size(152, 49);
             this.dataGridView1.TabIndex = 1;
             // 
             // labelTitle
             // 
             this.labelTitle.AutoSize = true;
-            this.labelTitle.Location = new System.Drawing.Point(12, 25);
+            this.labelTitle.Location = new System.Drawing.Point(12, 9);
             this.labelTitle.Name = "labelTitle";
             this.labelTitle.Size = new System.Drawing.Size(35, 13);
             this.labelTitle.TabIndex = 2;
             this.labelTitle.Text = "label1";
+            // 
+            // flowPanelFiles
+            // 
+            this.flowPanelFiles.AutoScroll = true;
+            this.flowPanelFiles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowPanelFiles.Location = new System.Drawing.Point(0, 0);
+            this.flowPanelFiles.Name = "flowPanelFiles";
+            this.flowPanelFiles.Size = new System.Drawing.Size(1144, 525);
+            this.flowPanelFiles.TabIndex = 3;
             // 
             // User_RepoProp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1144, 592);
+            this.Controls.Add(this.flowPanelFiles);
             this.Controls.Add(this.labelTitle);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.BtnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "User_RepoProp";
             this.Text = "User_RepoProp";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -82,5 +96,6 @@
         private System.Windows.Forms.Button BtnExit;
         public System.Windows.Forms.DataGridView dataGridView1;
         public System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelFiles;
     }
 }
--- User_RepoProp.cs
+++ User_RepoProp.cs
@@ -24,7 +24,76 @@ namespace git_hub_app
             labelTitle.Text = "Repository Details";
         }
 
-     
+
+
+        public void ShowFileCards(List<(string Name, string Type, string Updated)> files)
+        {
+            flowPanelFiles.Controls.Clear();
+
+            foreach (var file in files)
+            {
+                var card = CreateFileCard(file.Name, file.Type, file.Updated);
+                flowPanelFiles.Controls.Add(card);
+            }
+        }
+        private Panel CreateFileCard(string name, string type, string updated)
+        {
+            Panel card = new Panel
+            {
+                Width = 350,
+                Height = 60,
+                BackColor = Color.WhiteSmoke,
+                BorderStyle = BorderStyle.FixedSingle,
+                Margin = new Padding(8),
+                Cursor = Cursors.Hand
+            };
+
+            PictureBox icon = new PictureBox
+            {
+                Width = 28,
+                Height = 28,
+                Location = new Point(10, 15),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = type == "dir"
+                    ? Properties.Resources.folder // add your own icon
+                    : Properties.Resources.fileIcon   // add your own icon
+            };
+
+            Label lblName = new Label
+            {
+                Text = name,
+                Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                Location = new Point(50, 8),
+                AutoSize = true
+            };
+
+            Label lblUpdated = new Label
+            {
+                Text = updated,
+                Font = new Font("Segoe UI", 8, FontStyle.Italic),
+                ForeColor = Color.DimGray,
+                Location = new Point(50, 30),
+                AutoSize = true
+            };
+
+            card.Controls.Add(icon);
+            card.Controls.Add(lblName);
+            card.Controls.Add(lblUpdated);
+
+            card.MouseEnter += (s, e) => card.BackColor = Color.Gainsboro;
+            card.MouseLeave += (s, e) => card.BackColor = Color.WhiteSmoke;
+
+            // Optional: Card click to open or preview file
+            card.Click += (s, e) =>
+            {
+                MessageBox.Show($"You clicked {name}", "File", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            };
+
+            return card;
+        }
+
+
+
 
         private void BtnExit_Click(object sender, EventArgs e)
         {
